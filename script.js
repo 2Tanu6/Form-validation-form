fa9ddf4d66503d18d3280dd9d7c79bcbd5707467
2Tanu6 <106528566+2Tanu6@users.noreply.github.com>
2024-01-30 05:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,8 +55,6 @@ function checkLength(input, min, max){
 function checkPasswordMatch(input1 , input2){
     if(input1.value !== input2.value){
         showError(input2, 'Passwords do not match');
-    }else{
-        showSuccess(input2);
     }
 }
 
